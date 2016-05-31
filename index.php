cbcbd61a348c135e259c1553c7a302222c4a34bf
Rob1Lab <rob1.labaune@gmail.com>
2016-05-31 13:13:16 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>CO-PRODUCED</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+     
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+    
+<body>
+<nav class="navbar navbar-default">
+    <div class="nav-header"></div>
+</nav>
+<div class="grid-960">
+
+
+<div class ="row">
+    <div class="col-md-8">
+        <h2>Projet trop soins</h2>
+        <p>Vide, quaeso, rectumne sit. Compensabatur, inquit, cum summis doloribus laetitia. Bonum valitudo: miser morbus. Ut pulsi recurrant? Scrupulum, inquam, abeunti; Facillimum id quidem est, inquam.</p>
+        <img src="images/table_de_mix.jpg" class="img-responsive" />
+    </div>
+    <div class="col-md-4">
+        <p class="total_projet" href="">23 750</p>
+        <p class="committted">committed on a goal of</p>
+        <p class="montant_projet" >25 000</p>
+    </div>
+    
+</div>
+
+
+
+</div>
+</body>
+</html>
